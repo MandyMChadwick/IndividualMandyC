833e2394fbf4d64cc4d9172816ad7ca8c3bd9803
MandyChadwick <cha33dwick@gmail.com>
2024-01-27 07:10:51 -0900

    changeed to right folders

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Cards from './components/cards'
+
+function App() {
+  return (
+    <div className="App">
+     <div className="title-container">
+      <br>
+      </br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      
+        <h1>Minding Babies</h1>
+      </div>
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
