43993bf8d9cc0bb415e8ccb5ba2506bf8fa98e36
MandyChadwick <cha33dwick@gmail.com>
2024-01-31 15:31:07 -0900

    responsive Navbar is created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+import "./Navbar.css";
+import { Link, NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav>
+      <Link to="/" className="title">
+        Home
+      </Link>
+      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={menuOpen ? "open" : ""}>
+        <li>
+          <NavLink to="/game1">Dad Jokes</NavLink>
+        </li>
+        <li>
+          <NavLink to="/game2">Drawing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/game3">Nursery Rhymnes</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+// // Navbar.js
+
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// function Navbar() {
+//   return (
+//     <nav id="main-navbar">
+//       <div id="navbar-container">
+//         <Link to="/" id="brand-link">Memory Game</Link>
+//         <br />
+//         <Link to="/game1" id="game1-link">Dad Jokes</Link>
+//         <br />
+//         <Link to="/game2" id="game2-link">Drawing Place</Link>
+//         <br />
+//         <Link to="/game3" id="game3-link">Nursery Rhymes</Link>
+//       </div>
+//     </nav>
+//   );
+// }
+
+export default Navbar;
